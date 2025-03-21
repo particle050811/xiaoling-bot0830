cc709d725332117be45b87fe31f77e36002a36f7
particle050811 <2667013482@qq.com>
2025-03-21 13:48:44 +0800

    0.0.23

--- qq-bot.py
+++ qq-bot.py
@@ -103,7 +103,7 @@ class Guild:
         self.role_dict = {sf.name: sf.id for sf in self.roles}
 
         self.formal_id = self.role_dict['正式成员']
-        self.smartboy_id = self.role_dict['违规发帖-请先看公告']
+        self.smartboy_id = self.role_dict['违规发帖-看公告-选择互助区发帖']
 
         #self.log_id = self.channel_dict['机器人运行日志']
         self.assessment_id = self.channel_dict['AI自动审核区']
@@ -209,6 +209,57 @@ class Messager:
                 reply+=chunk
             bot.logger.info(reply)
 
+class Forumer:
+     def __init__(self,data: Model.FORUMS_EVENT):
+         self.author_id=data.author_id
+         self.thread_id=data.thread_info.thread_id
+         self.channel_id=data.channel_id
+         self.user=bot.api.get_member_info(data.guild_id,data.author_id).data
+ 
+         self.head=f'<@{self.author_id}>\n'
+ 
+     def reply(self,msg:str):
+         bot.api.send_msg(channel_id=guild.assessment_id,
+                          content=self.head+msg,
+                          message_id=self.thread_id)
+     def is_legal(self):
+         for channel in guild.channels:
+             if channel.name=='帖子广场' and channel.id==self.channel_id:
+                 return False
+         return True
+     def is_formal(self):
+         return guild.formal_id in self.user.roles
+     def is_admin(self):
+         return set(guild.admin_ids)&set(self.user.roles)
+     def delete(self):
+         bot.api.delete_thread(self.channel_id,self.thread_id)
+     def remind(self):
+         bot.api.create_role_member(self.author_id,guild.id,guild.smartboy_id)
+         bot.api.delete_role_member(self.author_id,guild.id,guild.smartboy_id)
+     def log(self,content):
+         bot.api.send_msg(channel_id=guild.log_id,
+                          content=content,
+                          message_id=self.thread_id)
+     def check(self):
+         if self.is_legal():
+             return
+         self.remind()
+         bot.logger.info(f'{self.user.user.username}非法发帖')
+ 
+         if self.is_formal():
+             self.reply((
+                 '机器人将在4分钟后把你在帖子广场的帖删除。'
+                 f'请在发帖选择<#{guild.cooperation_id}>板块，不要到帖子广场发帖。'
+             ))
+             threading.Timer(240, self.delete).start()
+         else:
+             self.reply((
+                 '机器人已自动将你在帖子广场的帖删除。'
+                 '由于很多人的举报信息收集表填写不完整，导致互助效率极度低下，'
+                 '故本频道需要通过考核后才能发帖。'
+                 '请先看公告，再来考核区参与考核。'
+             ))
+             self.delete() 
 
 with open('../qq-bot.json', 'r', encoding='utf-8') as file:
     cg = json.load(file)
@@ -231,6 +282,16 @@ def deliver(data: Model.MESSAGE):
     msg.check()
     msg.query()
 
+@bot.bind_forum()
+def forum_function(data: Model.FORUMS_EVENT):
+     if data.t != 'FORUM_THREAD_CREATE':
+         return
+     if not guild.set(data.guild_id):
+         return 
+     if data.guild_id!=guild.id:
+         return
+     user=Forumer(data)
+     user.check()
 
 if __name__ == "__main__":
     bot.start()
